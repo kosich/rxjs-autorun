d98b8b8ecff69707f8f8211d997b5adc6d84e511
Kostia P <kosich@users.noreply.github.com>
2020-10-21 12:39:48 +0300

    Refactoring to directly pushing to observer (#28)
    * refactored to direct push to the observer
    * const enums
    * renamed and moved things around
    * moved out the check for completion
    * dropped expression args
    * reusing ready context object
    * removed for-of constructs
    * fixed some comments
    * optimized halt case
    * optimized restoring lowered strength

--- src/autorun.ts
+++ src/autorun.ts
@@ -1,60 +1,25 @@
-import { EMPTY, Observable, of, Subject, Subscription, throwError } from 'rxjs';
-import { distinctUntilChanged, startWith, switchMap, takeWhile } from 'rxjs/operators';
+import { Observable, Subscription } from 'rxjs';
+import { distinctUntilChanged } from 'rxjs/operators';
 
 
-enum Strength {
-    Weak = 0,
-    Normal = 1,
-    Strong = 2
-}
+// an error to make mid-flight interruptions
+// when a value is still not available
+const HALT_ERROR = Object.create(null);
 
-interface TrackEntry<V> {
-    hasValue: boolean;
-    value?: V;
-    subscription?: Subscription;
-    track: boolean;
-    used: boolean;
-    completed: boolean;
-    strength: Strength;
-}
-
-const ERROR_STUB = Object.create(null);
-
-type $Fn = <T>(o: Observable<T>) => T;
-type Cb<T> = (...args: any[]) => T;
-
-type Trackers = {
-    weak: $Fn;
-    normal: $Fn;
-    strong: $Fn;
-}
-type $FnWithTrackers = $Fn & Trackers;
-
-enum Update {
-    Value,
-    Completion
-};
-
-export function autorun<T>(fn: Cb<T>) {
-    return computed<T>(fn).subscribe();
-}
-
-const errorTracker = (() => { throw new Error('$ or _ can only be called within computed or autorun context'); }) as any as $FnWithTrackers;
+// error if tracker is used out of autorun/computed context
+export const TrackerError = new Error('$ or _ can only be called within computed or autorun context');
+const errorTracker: Trackers = () => { throw TrackerError; };
 errorTracker.weak = errorTracker;
 errorTracker.normal = errorTracker;
 errorTracker.strong = errorTracker;
 
-type Context = {
-    _: $FnWithTrackers,
-    $: $FnWithTrackers
-}
 let context: Context = {
     _: errorTracker,
     $: errorTracker
 };
 
-const forwardTracker = (tracker: keyof Context): $FnWithTrackers => {
-    const r  = (<T>(o: Observable<T>): T => context[tracker](o)) as $FnWithTrackers;
+const forwardTracker = (tracker: keyof Context): Trackers => {
+    const r  = (<T>(o: Observable<T>): T => context[tracker](o)) as Trackers;
     r.weak   = o => context[tracker].weak(o);
     r.normal = o => context[tracker].normal(o);
     r.strong = o => context[tracker].strong(o);
@@ -64,102 +29,119 @@ const forwardTracker = (tracker: keyof Context): $FnWithTrackers => {
 export const $ = forwardTracker('$');
 export const _ = forwardTracker('_');
 
-export const computed = <T>(fn: Cb<T>): Observable<T> => new Observable(observer => {
+export function autorun<T>(fn: Expression<T>) {
+    return computed<T>(fn).subscribe();
+}
+
+export const computed = <T>(fn: Expression<T>): Observable<T> => new Observable<T>(observer => {
     const deps = new Map<Observable<unknown>, TrackEntry<unknown>>();
-    const update$ = new Subject<Update>();
-    const createTrackers = (track: boolean) => {
-        const r  = createTracker(track, Strength.Normal) as $FnWithTrackers;
+
+    // context to be used for running expression
+    const newCtx = {
+        $: createTrackers(true),
+        _: createTrackers(false)
+    };
+
+    // on unsubscribe/complete we destroy all subscriptions
+    const sub = new Subscription(() => {
+        deps.forEach(dep => {
+            dep.subscription.unsubscribe();
+        });
+    });
+
+    // flag that indicates that current run might've affected completion status
+    // we'll check completion after the first run
+    let shouldCheckCompletion = true;
+
+    // initial run
+    runFn();
+
+    return sub;
+
+
+    function runFn () {
+        // Mark all deps as untracked and unused, lowering normal to weak
+        const loweredStrengthDeps: TrackEntry<any>[] = [];
+        deps.forEach(dep => {
+            dep.track = false;
+            dep.used = false;
+            // setting normal strength to weak, so that if previously `a` was
+            // tracked as normal and in the latest run we only see `weak(a)` -
+            // we can mark it as weak. this is restored if halted
+            if (dep.strength === Strength.Normal) {
+                dep.strength = Strength.Weak;
+                loweredStrengthDeps.push(dep);
+            }
+        });
+
+        const prevCtxt = context;
+        context = newCtx;
+        try {
+            const result = fn();
+            removeUnusedDeps(Strength.Normal);
+            observer.next(result);
+        } catch (e) {
+            // handling mid-flight interruption error
+            // NOTE: check requires === strict equality
+            if (e === HALT_ERROR) {
+                // restore lowered strength
+                loweredStrengthDeps.forEach(dep => {
+                    dep.strength = Strength.Normal;
+                });
+                // clean-up weak subscriptions
+                removeUnusedDeps(Strength.Weak);
+            } else {
+                // rethrow original errors
+                observer.error(e);
+                // we're errored, no need to check completion
+                shouldCheckCompletion = false;
+            }
+        } finally {
+            context = prevCtxt;
+
+            // if this run was flagged as potentially completing
+            if (shouldCheckCompletion) {
+                checkCompletion();
+            }
+        }
+    }
+
+    function checkCompletion () {
+        // reset the flag
+        shouldCheckCompletion = false;
+
+        // any dep is still running
+        for (let dep of deps.values()) {
+            if (dep.track && !dep.completed) {
+                // one of the $-tracked deps is still running
+                return;
+            }
+        }
+
+        // All $-tracked deps completed
+        observer.complete();
+    }
+
+    function removeUnusedDeps (ofStrength: Strength) {
+        deps.forEach((dep, key) => {
+            if (dep.used || dep.strength > ofStrength) {
+                return;
+            }
+            dep.subscription.unsubscribe();
+            deps.delete(key);
+        });
+    }
+
+    function createTrackers (track: boolean) {
+        const r  = createTracker(track, Strength.Normal) as Trackers;
         r.weak   = createTracker(track, Strength.Weak);
         r.normal = createTracker(track, Strength.Normal);
         r.strong = createTracker(track, Strength.Strong);
         return r;
-    };
-    const $ = createTrackers(true);
-    const _ = createTrackers(false);
-
-    let error: any;
-    let hasError = false;
-
-    const anyDepRunning = () => {
-        for (let dep of deps.values()) {
-            if (dep.track && !dep.completed) {
-                // One of the $-tracked deps is still running
-                return true;
-            }
-        }
-        // All $-tracked deps completed
-        return false;
     }
 
-    const removeUnusedDeps = (ofStrength: Strength) => {
-        for (let [key, { used, subscription, strength }] of deps.entries()) {
-            if (used || strength > ofStrength) {
-                continue;
-            }
-            subscription?.unsubscribe();
-            deps.delete(key);
-        }
-    }
-
-    const runFn = () => {
-        if (hasError) {
-            return throwError(error);
-        }
-        // Mark all deps as untracked and unused
-        const maybeRestoreStrength: Observable<any>[] = [];
-        for (let [key, dep] of deps.entries()) {
-            dep.track = false;
-            dep.used = false;
-            if (dep.strength === Strength.Normal) {
-                // Reset normal strength to weak when last run was successfull.
-                dep.strength = Strength.Weak;
-                maybeRestoreStrength.push(key);
-            }
-        }
-        const prevCtxt = context;
-        context = {$, _};
-        try {
-            const rsp = fn();
-            removeUnusedDeps(Strength.Normal);
-            return of(rsp);
-        } catch (e) {
-            for (let dep of maybeRestoreStrength) {
-                deps.get(dep)!.strength = Strength.Normal;
-            }
-            removeUnusedDeps(Strength.Weak);
-            // rethrow original errors
-            // NOTE: check requires === if e is primitive, JS engine will try to
-            //       convert ERROR_STUB to primitive
-            if (e !== ERROR_STUB) {
-                throw e;
-            }
-            return hasError ? throwError(error) : EMPTY;
-        } finally {
-            context = prevCtxt;
-        }
-    };
-
-    const sub = update$
-        .pipe(
-            // run fn() and completion checker instantly
-            startWith(Update.Value, Update.Completion),
-            takeWhile(u => u !== Update.Completion || anyDepRunning()),
-            switchMap(u => u === Update.Value ? runFn() : EMPTY),
-            distinctUntilChanged(),
-        )
-        .subscribe(observer);
-
-    // destroy all subscriptions
-    sub.add(() => {
-        deps.forEach((entry) => {
-            entry.subscription?.unsubscribe();
-        });
-        update$.complete();
-        deps.clear();
-    });
-
-    function createTracker(track: boolean, strength: Strength) {
-        return function $<O>(o: Observable<O>): O {
+    function createTracker(track: boolean, strength: Strength): Tracker {
+        return function tracker<O>(o: Observable<O>): O {
             if (deps.has(o)) {
                 const v = deps.get(o)!;
                 v.used = true;
@@ -177,74 +159,125 @@ export const computed = <T>(fn: Cb<T>): Observable<T> => new Observable(observer
                 if (v.hasValue) {
                     return v.value as O;
                 } else {
-                    throw ERROR_STUB;
+                    throw HALT_ERROR;
                 }
             }
 
             const v: TrackEntry<O> = {
                 hasValue: false,
                 value: void 0,
-                subscription: void 0,
+                // Eagerly create subscription that can be destroyed.
+                subscription: new Subscription(),
+                strength,
                 track: true,
                 used: true,
-                completed: false,
-                strength
+                completed: false
             };
 
+            deps.set(o, v);
+
+            // Sync Code Section {{{
             // NOTE: we will synchronously (immediately) evaluate observables
             // that can synchronously emit a value. Such observables as:
             // - of(…)
-            // - timer(0, …)
             // - o.pipe( startWith(…) )
             // - BehaviorSubject
             // - ReplaySubject
             // - etc
             let isAsync = false;
-            v.subscription = o
-                .pipe(distinctUntilChanged())
+            let hasSyncError = false;
+            let syncError = void 0;
+            v.subscription.add(
+                o.pipe(distinctUntilChanged())
                 .subscribe({
-                    next: (value) => {
+                    next(value) {
                         const hadValue = v.hasValue;
                         v.hasValue = true;
                         v.value = value;
 
-                        if (isAsync && v.track) {
-                            update$.next(Update.Value);
+                        const isUntrackFirstValue = !hadValue && !track;
+
+                        // It could be that all tracked deps already completed.
+                        // So signal that completion state might have changed.
+                        if (isUntrackFirstValue) {
+                            shouldCheckCompletion = true;
                         }
-                        if (!hadValue && !track) {
+
+                        if (isAsync && v.track) {
+                            runFn();
+                        }
+
+                        if (isUntrackFirstValue) {
                             // Untracked dep now has it's first value. So really untrack it.
                             v.track = false;
-                            // It could be that all tracked deps already completed. So signal
-                            // that completion state might have changed.
-                            update$.next(Update.Completion);
                         }
                     },
-                    error: e => {
-                        error = e;
-                        hasError = true;
+                    error(err) {
                         if (isAsync) {
-                            update$.next(Update.Value);
+                            observer.error(err);
+                        } else {
+                            syncError = err;
+                            hasSyncError = true;
                         }
                     },
-                    complete: () => {
+                    complete() {
                         v.completed = true;
                         if (isAsync && v.track) {
-                            update$.next(Update.Completion);
+                            checkCompletion();
                         }
                     }
-                });
+                })
+            );
+            if (hasSyncError){
+                throw syncError;
+            }
             isAsync = true;
-
-            deps.set(o, v);
+            // }}} End Of Sync Section
 
             if (v.hasValue) {
                 // Must have value because v.hasValue is true
                 return v.value!;
             } else {
-                throw ERROR_STUB;
+                throw HALT_ERROR;
             }
         };
     }
+})
+// distinct results for computed
+.pipe(distinctUntilChanged());
 
-    return sub;
-});
+type Expression<T> = () => T;
+
+interface TrackEntry<V> {
+    hasValue: boolean;
+    value?: V;
+    /** subscription to source */
+    subscription: Subscription;
+    /** subscription strength */
+    strength: Strength;
+    /** is tracked $ or untracked _ */
+    track: boolean;
+    /** has been used in latest run */
+    used: boolean;
+    /** source completion status */
+    completed: boolean;
+}
+
+const enum Strength {
+    Weak = 0,
+    Normal = 1,
+    Strong = 2
+}
+
+interface Context {
+    _: Trackers;
+    $: Trackers;
+}
+
+interface Trackers extends Tracker {
+    weak: Tracker;
+    normal: Tracker;
+    strong: Tracker;
+}
+
+type Tracker = <T>(o: Observable<T>) => T;
